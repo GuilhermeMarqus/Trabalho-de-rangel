6b98cdf2b5518a67b543b1fb403d311661021a33
GuilhermeMarqus <gjanuario403@gmail.com>
2025-02-03 19:24:36 -0300

    Realizando auterações em meu projeto

--- README.md
+++ README.md
@@ -22,3 +22,6 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+Realizando alterações de testes para o repositorio
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,29 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      Crie sua conta no Nosso banco <br>
-      e <br>
-      Deposite sua primeira renda acima de 100 reais
-    </p>
-
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+    <div class="hello">
+      <h1>{{ msg }}</h1>
+      <p>
+        Crie sua conta no Nosso banco <br>
+        e <br>
+        Deposite sua primeira renda acima de 100 reais
+      </p>
+  
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    }
   }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-p {
-  color: #2c3e50;
-  text-transform: uppercase;
-}
-</style>
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  p {
+    color: #2c3e50;
+    text-transform: uppercase;
+  }
+  </style>
+  
